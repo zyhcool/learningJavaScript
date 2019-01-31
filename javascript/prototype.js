3c0fff99bc07ff176e0d7d34c41053feb63ec6d8
zyh <13105027620@163.com>
2019-01-31 23:58:22 +0800

    cdf

--- javascript/prototype.js
+++ javascript/prototype.js
@@ -1,13 +1,16 @@
 let Person = function(){
-    return Person.prototype.init();
+    // return Person.prototype.init();
 }
 
 Person.prototype = {
-    init: function(){
+    init: function(name){
+        this.name = name ? name : this.name;
         return this;
     },
     name: "asa",
 }
 
-let person = new Person();
+let person = new Person().init("hh");
+// person.init();
+console.log(person);
 console.log(person.name);
