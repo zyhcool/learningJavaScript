502fed664b68e4736a5ab8cdf8c0368a91d4247f
zyh <13105027620@163.com>
2019-04-15 20:07:01 +0800

    d

--- 浏览器渲染机制/hello.js
+++ 浏览器渲染机制/hello.js
@@ -1,5 +1,10 @@
-console.log("from hello.js");
-a = "hahah";
-setTimeout(() => {
-    console.log("settimeout");
-}, 3000);
+
+        let list = document.querySelector("ul");
+        let items = [1,2,3,45,2];
+        var fragment = document.createDocumentFragment();
+        for (var i = 0; i < items.length; i++) {
+            var item = document.createElement("li");
+            item.appendChild(document.createTextNode("Option " + i));
+            fragment.appendChild(item);
+        }
+        list.appendChild(fragment);
