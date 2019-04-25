0e69644955d1b75b102d99654b328911b8a2bc21
zyh <13105027620@163.com>
2019-04-25 15:35:13 +0800

    sds

--- javascript/test.js
+++ javascript/test.js
@@ -1,32 +1,16 @@
-let data = `
-As a boy I dreamed of flying airplanes. 21__________I told Dad I wanted to be a pilot,he said, "Youneed to do your job well on the farm first, and 22__________you may realize your dream. "The farm was aneveryday responsibility (职责)--that. 23__________Saturday, Sunday and Christmas too. Every morning Igot up at four o' clock to milk and 24__________the cows. After the milk was bottled, my older brothers andI 25__________it to people' s homes. I was paid five cents a bottle. At that time I 26__________that to be success-ful you have to take care of your responsibilities.
-When I was 16 ,I signed up for flying lessons. To pay for the lessons I 27__________a small restaurantwith some friends. One of our regular 28__________was the president of the Farmers Bank. Every 29__________Igot,I talked to him about my dream. One day I told him I 30__________$3,500 to buy my own 31__________. Helent me the money so that I could fly people within 150 miles of the airport.
-Twelve years later I was flying people around the world 32__________a pilot of a big airline company.
-After ten years in the company I started my own 33__________.
-Today I run a billion-dollar company, but it was on the farm that I learned the principles(原则)that have 34__________me. Hard work and determination do 35__________dreams come true.
 
-21. 请选择最佳选项( )。
-A. Before
-B. When
-C. Since
-D. Though
-答案：B
-解析：词义辨析题。这句话的意思是：当我告诉爸爸我想成为一名飞行员时，他说：“你应该先做好农场上的工作，然后你才会实现你的理想。”when引导时间状语从句，表示“当……时”，故选B。
 
-22. 请选择最佳选项( )。
-A. again
-B. now
-C. then
-D. Still
-答案：C
-解析：词义辨析题。again意为“又，再”;now意为“现在”;then意为“然后，那时”;still意为“仍然，仍旧”。根据句意可知，C为正确答案。
+let arr = [1, 3, 5];
+let promises = arr.map(async (num) => {
+    return await returnSomething(num);
+})
 
-23. 请选择最佳选项( )。
-A. wasted
-B. spent
-C. followed
-D. Meant
-答案：D
-解析：词义辨析题。农场是每日的责任，也就是说，即使是周六、周日和圣诞节也一样。mean意为“表示……意思，意味着……”，符合句意。
-
-`
+async function returnSomething(num) {
+    if (num === 3) {
+        for (let i = 0; i < 2 * (10 ** 9); i++) { }
+        console.log(num);
+    } else {
+        console.log(num);
+    }
+}
+await Promise.all(promises);
