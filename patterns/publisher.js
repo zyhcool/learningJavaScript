164d50575654fe4534820725408f72e924fa7b67
zyh <13105027620@163.com>
2019-08-19 09:53:33 +0800

    publish-subscrib pattern

--- patterns/publisher.js
+++ patterns/publisher.js
@@ -1,22 +1,78 @@
-class Shop {
+// class Shop {
+//     constructor() {
+//         this.guests = [];
+//     }
+//     listen(fn) {
+//         this.guests.push(fn);
+//     }
+//     publish() {
+//         for (let i = 0; i < this.guests.length; i++) {
+//             this.guests[i].apply(this, arguments);
+//         }
+//     }
+// }
+
+// let shoeshop = new Shop();
+
+// shoeshop.listen((color, size) => {
+//     console.log("color: ", color);
+//     console.log("size: ", size);
+// })
+
+// shoeshop.publish("red", 23);
+
+
+
+
+
+class Publisher {
     constructor() {
-        this.guests = [];
+        this.topics = {};
     }
-    listen(fn) {
-        this.guests.push(fn);
-    }
-    publish() {
-        for (let i = 0; i < this.guests.length; i++) {
-            this.guests[i].apply(this, arguments);
+
+    subscrib(topic, fnc) {
+        if (!this.topics[topic]) {
+            this.topics[topic] = [];
         }
+        const key = Math.random();
+        this.topics[topic].push({
+            key,
+            fnc,
+        })
+        return key;
+    }
+
+    unSubscrib(topic, key) {
+        if (!this.topics[topic]) {
+            return;
+        }
+        this.topics[topic].forEach((v, i) => {
+            if (v.key === key) {
+                this.topics[topic].splice(i, 1);
+            }
+        })
+    }
+
+    publish(topic) {
+        if (!this.topics[topic] || this.topics[topic].length === 0) {
+            return;
+        }
+        this.topics[topic].forEach(item => item.fnc())
     }
 }
 
-let shoeshop = new Shop();
+let publisher = new Publisher();
 
-shoeshop.listen((color, size) => {
-    console.log("color: ", color);
-    console.log("size: ", size);
+let xh = publisher.subscrib("success", () => {
+    console.log("success");
 })
 
-shoeshop.publish("red", 23);
+let xm = publisher.subscrib("error", () => {
+    console.log("error");
+})
+
+
+publisher.unSubscrib("success",xm)
+
+publisher.publish("success")
+publisher.publish("error")
